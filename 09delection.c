26867f3f28d5ac6cd86217f1a994545ab1d76ba9
Sahilshowkat786 <sahilshowkatsofi@gmail.com>
2025-09-05 15:38:01 +0530

    03-09-2025

---
+++ 09delection.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int arr[100], n;
+    printf("enter no of elements = ");
+    scanf("%d", &n);
+    printf("enter %d elements ", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    n--;
+    printf("after deletion of first element output array is = ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    printf("after deletion of last element output array is = ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+    }
+    return 0;
+}
